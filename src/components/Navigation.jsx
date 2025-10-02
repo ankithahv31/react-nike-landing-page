8596b46de9bd32b6b0fabe43d8e0a2de92d63289
Ankitha <ankithahv31@gmail.com>
2025-10-02 22:47:23 +0530

    React based Nike landing page UI

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div> <nav className='navigation'>
+      <div className="logo"><img src="/images/brand_logo.png" alt="logoimage" /></div>
+     <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+     </ul>
+     <button>Login</button>
+     </nav></div>
+  )
+}
+
+export default Navigation
