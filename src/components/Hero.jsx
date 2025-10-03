6ed2c4a97ad7d6599654b4a196a4ada3f9422250
Ankitha <ankithahv31@gmail.com>
2025-10-03 22:06:59 +0530

    paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <main className='hero container'>
         <div className='hero-content'>
             <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>YOUR FEET DESERVE THE BEST WE'RE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST WE'RE HERE TO HELP YOU WITH OUR SHOES. </p>
+            <p>YOUR FEET DESERVE THE BEST WE'RE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST WE'RE HERE TO HELP YOU WITH OUR SHOES..</p>
         
         <div className='hero-btn'>
             <button>Shop Now</button>
